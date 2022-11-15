95c068d9c122552bda37d9ce7e97ece613bec311
fewerner <felix@wernerhome.de>
2022-11-15 15:12:33 +0100

    refactor: separate eid client from eid state

--- traits/src/state.rs
+++ traits/src/state.rs
@@ -17,15 +17,6 @@ pub trait EidState {
     /// Verify that a client is part of the EID.
     fn verify_client(&self, client: Client) -> Result<bool, EidError>;
 
-    /// Add a client to the EID.
-    fn add(&self) -> Result<Self::Evolvement, EidError>;
-
-    /// Remove a client from the EID.
-    fn remove(&self) -> Result<Self::Evolvement, EidError>;
-
-    /// Update your own key material.
-    fn update(&self) -> Result<Self::Evolvement, EidError>;
-
     /// Get all clients which are members of the EID.
     fn get_clients(&self) -> Result<Vec<Client>, EidError>;
 
--- traits/src/traits.rs
+++ traits/src/traits.rs
@@ -1,14 +1,28 @@
+use crate::types::EidError;
+
 pub mod state;
 pub mod key_store;
 pub mod types;
 pub mod evolvement;
 mod transcript;
 
-pub trait EidProvider {
+pub trait EidClient {
     type StateProvider: state::EidState;
     type KeyStoreProvider: key_store::EidKeyStore;
 
     fn state(&self) -> &Self::StateProvider;
 
-    fn key_store(&self) -> &Self::KeytoreProvider;
+    fn key_store(&self) -> &Self::KeyStoreProvider;
+
+    /// Create the first [EidState] of an EID by interacting with a PKI. We assume trust on first use on the resulting [EidState].
+    fn create(&self) -> Result<(), EidError>;
+
+    /// Create an [Evolvement] to add a client to the EID.
+    fn add(&self) -> Result<Self::Evolvement, EidError>;
+
+    /// Create an [Evolvement] to remove a client from the EID.
+    fn remove(&self) -> Result<Self::Evolvement, EidError>;
+
+    /// Create an [Evolvement] to update your own key material.
+    fn update(&self) -> Result<Self::Evolvement, EidError>;
 }
