4331f08311f06e5cbe7ddaeed5818e006e7d6d69
fewerner <felix@wernerhome.de>
2022-12-02 11:11:22 +0100

    style: ran cargo fmt

--- traits/src/traits.rs
+++ traits/src/traits.rs
@@ -1,6 +1,6 @@
-pub mod state;
-pub mod key_store;
-pub mod types;
-pub mod evolvement;
-pub mod transcript;
 pub mod client;
+pub mod evolvement;
+pub mod key_store;
+pub mod state;
+pub mod transcript;
+pub mod types;
