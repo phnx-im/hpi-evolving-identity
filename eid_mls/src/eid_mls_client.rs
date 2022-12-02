d8f838ea5683ac12800a397fd0897481260b0cc3
fewerner <felix@wernerhome.de>
2022-12-02 11:17:11 +0100

    refactor: adjusted mls client scratch to current EidClient trait

--- eid_mls/src/eid_mls_client.rs
+++ eid_mls/src/eid_mls_client.rs
@@ -2,21 +2,34 @@ use eid_traits::client::EidClient;
 use eid_traits::types::{EidError, Member};
 
 #[derive(Default)]
-struct EidMlsClient {
-
-}
+struct EidMlsClient {}
 
 impl EidClient for EidMlsClient {
-    type StateProvider = Type;
-    type KeyStoreProvider = Type;
-    type EvolvementProvider = Type;
+    type StateProvider = Type; // Todo
+    type KeyStoreProvider = Type; // Todo
+    type EvolvementProvider = Type; // Todo
 
-    fn default() -> Self { todo!() }
-    fn state(&self) -> &<Self as EidClient>::StateProvider { todo!() }
+    fn state(&self) -> &<Self as EidClient>::StateProvider {
+        todo!()
+    }
 
-    fn key_store(&self) -> &<Self as EidClient>::KeyStoreProvider { todo!() }
-    fn create_eid(&self) -> Result<(), EidError> { todo!() }
-    fn add(&self, _: Member) -> Result<<Self as EidClient>::EvolvementProvider, EidError> { todo!() }
-    fn remove(&self, _: Member) -> Result<<Self as EidClient>::EvolvementProvider, EidError> { todo!() }
-    fn update(&self) -> Result<<Self as EidClient>::EvolvementProvider, EidError> { todo!() }
+    fn key_store(&self) -> &<Self as EidClient>::KeyStoreProvider {
+        todo!()
+    }
+    fn create_eid(keystore: Self::KeyStoreProvider) -> Result<(), EidError> {
+        todo!()
+    }
+    fn add(&self, _: Member) -> Result<<Self as EidClient>::EvolvementProvider, EidError> {
+        todo!()
+    }
+    fn remove(&self, _: Member) -> Result<<Self as EidClient>::EvolvementProvider, EidError> {
+        todo!()
+    }
+    fn update(&self) -> Result<<Self as EidClient>::EvolvementProvider, EidError> {
+        todo!()
+    }
+
+    fn pk(&self) -> &Vec<u8> {
+        todo!()
+    }
 }
