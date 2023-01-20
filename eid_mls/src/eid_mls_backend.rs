fafd3e2595b7c8a2519cd02637efceb8dd4063cb
Konrad Lux <konrad.lux@student.hpi.de>
2023-01-20 14:15:36 +0100

    chore: Changes by Konrad

--- eid_mls/src/eid_mls_backend.rs
+++ eid_mls/src/eid_mls_backend.rs
@@ -10,6 +10,7 @@ impl Default for EidMlsBackend {
     fn default() -> Self {
         Self {
             mls_backend: OpenMlsRustCrypto::default(),
+            ciphersuite: Ciphersuite::MLS_128_DHKEMX25519_AES128GCM_SHA256_Ed25519,
         }
     }
 }
