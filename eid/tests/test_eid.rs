32fee50a4ae8f4fa041f117a6b376f8a99bf8d4b
Konrad Lux <konrad.lux@student.hpi.de>
2023-01-24 14:48:45 +0100

    chore: Changes by Konrad

--- eid/tests/test_eid.rs
+++ eid/tests/test_eid.rs
@@ -46,11 +46,14 @@ where
 
     // Create Alice as a member with a random pk
     let alice = C::generate_initial_id(backend);
-    let add_alice_evolvement = client.add(&alice, backend).expect("failed to add member");
+    let add_alice_evolvement_out = client.add(&alice, backend).expect("failed to add member");
 
     // member list length unchanged before evolving
     let members = client.get_members();
     assert_eq!(1, members.len());
+
+    let add_alice_evolvement_in: C::EvolvementProvider =
+        simulate_transfer(&add_alice_evolvement_out);
 
     client
         .evolve(add_alice_evolvement.clone(), backend)
@@ -73,9 +76,10 @@ where
 
     // Add Bob
     let bob = C::generate_initial_id(backend);
-    let add_bob_evolvement = client.add(&bob, backend).expect("failed to add member");
+    let add_bob_evolvement_out = client.add(&bob, backend).expect("failed to add member");
+    let add_bob_evolvement_in: C::EvolvementProvider = simulate_transfer(&add_bob_evolvement_out);
     client
-        .evolve(add_bob_evolvement.clone(), backend)
+        .evolve(add_bob_evolvement_in.clone(), backend)
         .expect("Failed to apply state");
 
     assert!(add_alice_evolvement_in.is_valid_successor(&add_bob_evolvement_in));
@@ -146,12 +150,13 @@ where
 
     let alice_before_update_1 = &client.get_members()[0];
 
-    let update_evolvement_1 = client.update(backend).expect("Updating client keys failed");
+    let update_evolvement_1_out = client.update(backend).expect("Updating client keys failed");
+    let update_evolvement_1_in: C::EvolvementProvider = simulate_transfer(&update_evolvement_1_out);
     client
-        .evolve(update_evolvement_1.clone(), backend)
+        .evolve(update_evolvement_1_in.clone(), backend)
         .expect("Failed to apply update on client state");
     transcript
-        .add_evolvement(update_evolvement_1.clone(), backend)
+        .add_evolvement(update_evolvement_1_in.clone(), backend)
         .expect("Failed to add evolvement");
     assert_eq!(transcript.get_members(), client.get_members());
 
@@ -162,13 +167,14 @@ where
 
     // Update Alice a second time
     let alice_before_update_2 = &members_after_update_1[0];
-    let update_evolvement_2 = client.update(backend).expect("Updating client keys failed");
+    let update_evolvement_2_out = client.update(backend).expect("Updating client keys failed");
+    let update_evolvement_2_in: C::EvolvementProvider = simulate_transfer(&update_evolvement_2_out);
     client
-        .evolve(update_evolvement_2.clone(), backend)
+        .evolve(update_evolvement_2_in.clone(), backend)
         .expect("Failed to apply update on client state");
-    assert!(update_evolvement_1.is_valid_successor(&update_evolvement_2));
+    assert!(update_evolvement_1_in.is_valid_successor(&update_evolvement_2_in));
     transcript
-        .add_evolvement(update_evolvement_2.clone(), backend)
+        .add_evolvement(update_evolvement_2_in.clone(), backend)
         .expect("Failed to add evolvement");
     assert_eq!(transcript.get_members(), client.get_members());
 
@@ -201,7 +207,7 @@ where
 }
 
 #[cfg(feature = "test")]
-fn simulate_transfer<I: Serialize, O: Deserialize>(input: I) -> O {
+fn simulate_transfer<I: Serialize, O: Deserialize>(input: &I) -> O {
     let serialized = input.tls_serialize_detached().expect("Failed to serialize");
     O::tls_deserialize(&mut serialized.as_slice()).expect("failed to deserialize")
 }
