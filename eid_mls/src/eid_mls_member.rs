3bea5f79816d9ea8bca3ff5f7e8b0373676153c1
Konrad Lux <konrad.lux@student.hpi.de>
2023-01-25 13:47:59 +0100

    feat: include signature key in member, make key package optional
    As there is no way to get the key package from an existing MLS group, we can't create members otherwise

--- eid_mls/src/eid_mls_member.rs
+++ eid_mls/src/eid_mls_member.rs
@@ -7,22 +7,27 @@ use eid_traits::member::Member;
 pub struct EidMlsMember {
     // TODO: do we need a constant identifier here?
     pub(crate) mls_member: Option<MlsMember>,
-    pub(crate) key_package: KeyPackage,
+    pub(crate) key_package: Option<KeyPackage>,
+    pub(crate) signature_key: Vec<u8>,
 }
 
 impl Member for EidMlsMember {
     type IdentityProvider = KeyPackage;
 
     fn new(key_package: Self::IdentityProvider) -> Self {
+        let kp_signature_key = key_package.leaf_node().signature_key().as_slice().to_vec();
         Self {
             mls_member: None,
-            key_package,
+            key_package: Some(key_package),
+            signature_key: kp_signature_key,
         }
     }
 
     #[cfg(feature = "test")]
     fn get_identity(&self) -> Self::IdentityProvider {
-        self.key_package.clone()
+        self.key_package
+            .expect("Doesn't contain key package")
+            .clone()
     }
 }
 
@@ -31,7 +36,7 @@ impl EidMlsMember {
         self.mls_member = Some(mls_member);
     }
 
-    fn update_key_package(&mut self, new_key_package: <Self as Member>::IdentityProvider) {
-        self.key_package = new_key_package;
+    fn update_signature_key(&mut self, new_signature_key: Vec<u8>) {
+        self.signature_key = new_signature_key;
     }
 }
