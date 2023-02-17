fb6661860302709654806a97a0b8624696f6b078
Simon Thormeyer <simonthormeyer@gmail.com>
2023-02-17 11:50:57 +0100

    chore: cleanup

--- traits/src/backend.rs
+++ traits/src/backend.rs
@@ -1,5 +1,6 @@
 use crate::client::EidClient;
 
+/// Represents the interface to a provider of cryptographic functions.
 pub trait EidBackend: Default {
     #[cfg(feature = "test")]
     type ClientProvider: EidClient<BackendProvider = Self>;
