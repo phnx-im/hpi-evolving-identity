8983ab6365b093ef6daa1099f2f45685fa9b223d
fewerner <felix@wernerhome.de>
2023-02-21 14:07:13 +0100

    docs: added docstrings for eid state

--- traits/src/state.rs
+++ traits/src/state.rs
@@ -3,13 +3,27 @@ use crate::evolvement::Evolvement;
 use crate::member::Member;
 use crate::types::EidError;
 
+/// # EidState
+/// The state of the EID. Each Client and the Transcript will have their own [EidState]
 pub trait EidState: Sized {
+    /// Type of [Evolvement](Self::EvolvementProvider) that can be applied on this [EidState](Self)
     type EvolvementProvider: Evolvement;
+
+    /// Type of [Member](Self::MemberProvider) that EID holds.
     type MemberProvider: Member;
+
+    /// Type of [EidBackend](Self::BackendProvider) this [EidState](Self) uses.
     type BackendProvider: EidBackend;
 
-    /// Apply an [Evolvement], changing the [EidState]. If the [Evolvement]
-    /// is invalid, return an [EidError].
+    /// Apply an [Evolvement] resulting in the next [EidState].
+    ///
+    /// # Arguments
+    ///
+    /// * `evolvement`: An [Evolvement]
+    /// * `backend`: The [Self::BackendProvider]
+    ///
+    /// returns: Result<(), EidError> [EidError] if evolvement is invalid.
+    ///
     fn apply(
         &mut self,
         evolvement: Self::EvolvementProvider,
@@ -17,10 +31,18 @@ pub trait EidState: Sized {
     ) -> Result<(), EidError>;
 
     /// Verify that a [Member] is part of the EID.
+    ///
+    /// # Arguments
+    ///
+    /// * `member`: A potential [Member]
+    ///
+    /// returns: [bool]
+    ///
     fn verify_member(&self, member: &Self::MemberProvider) -> bool {
         self.get_members().contains(member)
     }
 
     /// Get all [Member]s of the EID.
+    /// returns: [Vec]<[Self::MemberProvider]>
     fn get_members(&self) -> Vec<Self::MemberProvider>;
 }
